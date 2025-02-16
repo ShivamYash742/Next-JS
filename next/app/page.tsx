ae2dee12a32a9937c101a49cdf56de46e404e375
Shivam Mishra <shivamyash742@gmail.com>
2025-02-16 22:25:26 +0530

    installation

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
-import App from './components/page'
+import App from "./components/page";
 
 export default function Home() {
   return (
-    <><h1 className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    Next JS
-    
-  </h1>
-  <App/>
-
-  </>
-    
+    <>
+      <h1 className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        Next JS
+      </h1>
+      <App />
+    </>
   );
 }
